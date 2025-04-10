6cf15fe975fe0c11ff281a49362fb88f811bcef0
ItsQuasi <truequasi2010@gmail.com>
2025-04-10 20:50:20 +0700

    A big update to my website
    Featuring a new design and now using tailwindcss.

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,3 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-let isDark = localStorage.getItem('darkMode') === 'enabled' ||
-    (!localStorage.getItem('darkMode') && window.matchMedia('(prefers-color-scheme: dark)').matches);
-
-// Apply initial theme
-document.body.classList.toggle('dark', isDark);
-document.documentElement.setAttribute("data-color-scheme", isDark ? "dark" : "light");
-
-function toggleDarkMode() {
-    isDark = !isDark;
-    document.body.classList.toggle('dark', isDark);
-    document.documentElement.setAttribute("data-color-scheme", isDark ? "dark" : "light");
-
-    // Force reflow without removing scrollbars
-    document.documentElement.classList.add('reflow');
-    document.documentElement.offsetHeight; // Trigger reflow
-    document.documentElement.classList.remove('reflow');
-
-    // Save mode
-    localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
-}
-
-if (darkModeToggle) {
-    darkModeToggle.addEventListener('click', toggleDarkMode);
-}
-
 function navbarMenu() {
     var navResponsive = document.getElementById("nav");
     if (navResponsive.className === "nav-menu") {
